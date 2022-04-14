f64a2a34cbcd0534a8ccb4838bf980c407306e71
Broxis <59177927+Broxis@users.noreply.github.com>
2022-04-14 16:02:07 +0200

    Commit Message

--- src/pages/MainPage.vue
+++ src/pages/MainPage.vue
@@ -108,7 +108,7 @@ export default {
   created() {
     axios.request({
       method: "GET",
-      url: "https://honorcolstcalculator.herokuapp.com/createToken",
+      url: "http://honorcolstcalculator.herokuapp.com/createToken",
     });
   },
   methods: {
@@ -336,7 +336,7 @@ export default {
       axios
         .request({
           method: "POST",
-          url: "https://honorcolstcalculator.herokuapp.com/getItemlevel",
+          url: "http://honorcolstcalculator.herokuapp.com/getItemlevel",
           headers: {},
           data: {
             charName: getName,
