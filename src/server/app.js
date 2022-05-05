a4e9dd11ec6038fb6c4820802e53156fe79adcbf
Broxis <59177927+Broxis@users.noreply.github.com>
2022-05-05 16:55:03 +0200

    Commit Message

--- src/server/app.js
+++ src/server/app.js
@@ -79,7 +79,7 @@ app.listen(process.env.PORT || 8080, () => {
   console.log(`Example app listening on port`);
 });
 
-await createToken();
+
 
 /*server.on('clientError', (err, socket) => {
   console.error(err);
