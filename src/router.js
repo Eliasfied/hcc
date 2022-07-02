bbe9c3c7868dbee8cd367dd28708f230df5c500d
Broxis <59177927+Broxis@users.noreply.github.com>
2022-07-02 13:07:38 +0200

    Commit Message

--- src/router.js
+++ src/router.js
@@ -2,7 +2,6 @@ import { createRouter, createWebHistory } from "vue-router";
 import MainPage from "./pages/MainPage.vue";
 import InfoPage from "./pages/InfoPage.vue";
 import ContactPage from "./pages/ContactPage.vue";
-import CharacterOutput from "../src/components/comp/CharacterOutput.vue";
 
 const router = createRouter({
   history: createWebHistory(),
